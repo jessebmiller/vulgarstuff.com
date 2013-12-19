6cfa600d12fb0bae448f1ed8001d6118f4becd95
Jesse B Miller <jesse@jessebmiller.com>
2013-12-18 18:02:26 -0800

    ready for deploy

---
+++ app/client/layout.js
@@ -0,0 +1,9 @@
+Template.fbSDK.loadSDK = function () {
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=268320759987353";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+};
